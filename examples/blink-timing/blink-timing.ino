b0c16283c6f745f2128d512c34cde80de14b9f69
boothinator <boothinator@users.noreply.github.com>
2021-12-22 12:43:20 -0500

    Blink timing

--- examples/blink-timing/blink-timing.ino
+++ examples/blink-timing/blink-timing.ino
@@ -1,14 +1,34 @@
 #include <timerUtil.h>
+#include <timerInterrupts.h>
+
+ticks16_t prevTicks = 0;
+
+void inputCaptureInterrupt(ticks16_t ticks)
+{
+  Serial.println(ticks - prevTicks);
+
+  prevTicks = ticks;
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // GetTimer(pin 8)
+  
+  // Configure the timer to run at the speed of the system clock
+  configureTimerClock(Timer::Timer1, TimerClock::ClkDiv1024);
+
+  // Put timer in Normal timing mode
+  configureTimerMode(Timer::Timer1, TimerMode::Normal);
+  
   pinMode(8, OUTPUT);
+
+  attachInputCaptureInterrupt(Timer::Timer1, inputCaptureInterrupt, Edge::Rising);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(8, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1);                       // wait for a second
+  delay(1000);             // wait for a second
   digitalWrite(8, LOW);    // turn the LED off by making the voltage LOW
-  delay(1);                       // wait for a second
+  delay(1000);             // wait for a second
 }
