f664f33485d4fc432ef41881b838972f6408e366
boothinator <boothinator@users.noreply.github.com>
2022-12-22 12:31:45 -0500

    fix test

--- test/test_extTimer/test_extTimer.cpp
+++ test/test_extTimer/test_extTimer.cpp
@@ -25,11 +25,19 @@
 char message[MAX_MESSAGE_LEN];
 
 void setUp(void) {
-  setTimerMode(TIMER1, TimerMode::Normal);
-  setTimerClock(TIMER1, TimerClock::Clk);
+  ExtTimer0.configure(TimerClock::Clk);
+  ExtTimer1.configure(TimerClock::Clk);
+  ExtTimer2.configure(TimerClock::Clk);
 }
 
 void tearDown(void) {
+}
+
+void test_configure()
+{
+  TEST_ASSERT_BIT_HIGH(TOIE0, TIMSK0);
+  TEST_ASSERT_BIT_HIGH(TOIE1, TIMSK1);
+  TEST_ASSERT_BIT_HIGH(TOIE2, TIMSK2);
 }
 
 void test_set()
@@ -56,28 +64,78 @@ void test_set()
   TEST_ASSERT_FALSE(tov);
 }
 
-void test_uniformIncreasing()
+void test_uniformIncreasing(ExtTimer &extTimer)
 {
-  ticksExtraRange_t prevTicks = ExtTimer1.get();
+  extTimer.resetOverflowCount();
+  ticksExtraRange_t prevTicks = extTimer.get();
+  ticksExtraRange_t prevOvfTicks = extTimer.getOverflowTicks();
 
-  for (unsigned long i = 0; i < 1000000; i++)
+  for (unsigned long i = 0; i < 10000 && prevOvfTicks < (1ul << 30); i++)
   {
-    ticksExtraRange_t curTicks = ExtTimer1.get();
-    TEST_ASSERT_GREATER_THAN(prevTicks, curTicks);
+    ticksExtraRange_t curTicks = extTimer.get();
+    ticksExtraRange_t curOvfTicks = extTimer.getOverflowTicks();
+
+    TEST_ASSERT_GREATER_OR_EQUAL_UINT32(prevTicks, curTicks);
+
+    TEST_ASSERT_GREATER_OR_EQUAL_UINT32(prevOvfTicks, curOvfTicks);
+    
     prevTicks = curTicks;
+    prevOvfTicks = curOvfTicks;
   }
+}
+
+void test_set8Bit()
+{
+  ticksExtraRange_t expected = 0xabcdef01;
+  ticks8_t expected8 = static_cast<ticks8_t>(expected);
+  ticksExtraRange_t expectedOverflowCount = (expected & 0xFFFFFF00) >> 8;
+
+  setTimerClock(TIMER0, TimerClock::None);
+
+  // Set TOV flag
+  TIFR0 = (1 << TOV0);
+
+  ExtTimer0.set(expected);
+
+  uint8_t tcnt = TCNT0;
+
+  TEST_ASSERT_EQUAL_UINT8(expected8, tcnt);
+  TEST_ASSERT_EQUAL_UINT32(expected, ExtTimer0.get());
+  TEST_ASSERT_EQUAL_UINT32(ExtTimer0.getOverflowCount(), expectedOverflowCount);
+
+  bool tov = (TIFR0 & (1 << TOV0)) == (1 << TOV0);
+
+  TEST_ASSERT_FALSE(tov);
+}
+
+void test_uniformIncreasing0()
+{
+  test_uniformIncreasing(ExtTimer0);
+}
+
+void test_uniformIncreasing1()
+{
+  test_uniformIncreasing(ExtTimer1);
+}
+
+void test_uniformIncreasing2()
+{
+  test_uniformIncreasing(ExtTimer2);
 }
 
 void setup() {
   // NOTE!!! Wait for >2 secs
   // if board doesn't support software reset via Serial.DTR/RTS
-  //delay(2000);
-  delay(100);
+  delay(2000);
 
   UNITY_BEGIN();    // IMPORTANT LINE!
 
+  RUN_TEST(test_configure);
   RUN_TEST(test_set);
-  RUN_TEST(test_uniformIncreasing);
+  RUN_TEST(test_uniformIncreasing1);
+  RUN_TEST(test_set8Bit);
+  RUN_TEST(test_uniformIncreasing0);
+  RUN_TEST(test_uniformIncreasing2);
 
   UNITY_END(); // stop unit testing
 }
