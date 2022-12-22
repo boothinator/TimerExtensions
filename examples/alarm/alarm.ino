95a39ecb43fbd17b2aed02813253e8647fda6801
boothinator <boothinator@users.noreply.github.com>
2022-12-22 17:41:56 -0500

    Alarm example

---
+++ examples/alarm/alarm.ino
@@ -0,0 +1,28 @@
+#include <timerAction.h>
+
+/*
+ * It's pretty easy to set an alarm, even one that's quite far in the future.
+ * At best, Arduino/AVR timers can set an alarm a little over 4 seconds into the
+ * future. We can use a TimerAction to set an alarm much further. Try it out!
+ */
+
+void alarm(TimerAction *, void *) {
+  Serial.println("Hello world!");
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  TimerAction1A.configure(TimerClock::ClkDiv8);
+
+  ticksExtraRange_t nowTicks = TimerAction1A.getNow();
+
+  ticksExtraRange_t alarmTicks = TimerAction1A.millisecondsToTicks(8000);
+
+  TimerAction1A.schedule(alarmTicks, alarm);
+
+  Serial.println("Alarm scheduled");
+}
+  
+void loop() {
+}
